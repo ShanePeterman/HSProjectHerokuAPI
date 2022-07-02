4f1d28b11f87a2644d522aaedde036f340dee7cb
Shane <shanetp0822@gmail.com>
2022-07-02 11:17:49 -0400

    turned debug off

--- HS Project copy/myproject/settings.py
+++ HS Project copy/myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7dxjf@p@fl4fj5pikn&)=1*@ip8(giv36epr0@fyaxbidpuwei'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
